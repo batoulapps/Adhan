6a9e88e48c4f21f70f7f8be3aade5dbddb010983
ahmedre <ahmedre@gmail.com>
2016-03-20 00:40:27 -0700

    Implement Math tests

--- java/adhan/src/main/java/com/batoulapps/adhan/internal/CalendricalHelper.java
+++ java/adhan/src/main/java/com/batoulapps/adhan/internal/CalendricalHelper.java
@@ -2,6 +2,7 @@ package com.batoulapps.adhan.internal;
 
 import org.threeten.bp.LocalDate;
 import org.threeten.bp.LocalDateTime;
+import org.threeten.bp.Year;
 
 class CalendricalHelper {
 
@@ -16,11 +17,21 @@ class CalendricalHelper {
     return julianDay(year, month, day, 0.0);
   }
 
+  /**
+   * The Julian Day for a given date
+   * @param localDate the date
+   * @return the julian day
+   */
   static double julianDay(LocalDate localDate) {
     return julianDay(localDate.getYear(),
         localDate.getMonthValue(), localDate.getDayOfMonth(), 0.0);
   }
 
+  /**
+   * The Julian Day for a given date and time
+   * @param localDateTime the date and time
+   * @return the julian day
+   */
   static double julianDay(LocalDateTime localDateTime) {
     return julianDay(localDateTime.getYear(), localDateTime.getMonthValue(),
         localDateTime.getDayOfMonth(), localDateTime.getHour() + localDateTime.getMinute() / 60.0);
@@ -51,22 +62,34 @@ class CalendricalHelper {
     return i0 + i1 + D + B - 1524.5;
   }
 
-  /* Julian century from the epoch. */
+  /**
+   * Julian century from the epoch.
+   * @param JD the julian day
+   * @return the julian century from the epoch
+   */
   static double julianCentury(double JD) {
     /* Equation from Astronomical Algorithms page 163 */
     return (JD - 2451545.0) / 36525;
   }
 
-  /* Whether or not a year is a leap year (has 366 days). */
+  /**
+   * Whether or not a year is a leap year (has 366 days)
+   * @param year the year
+   * @return whether or not its a leap year
+   */
   static boolean isLeapYear(int year) {
-    if (year % 4 != 0) {
-      return false;
-    }
+    return Year.isLeap(year);
+  }
 
-    if (year % 100 == 0 && year % 400 != 0) {
-      return false;
-    }
-
-    return true;
+  /**
+   * Local date and time with a rounded minute
+   * This returns a date with the seconds rounded and added to the minute
+   * @param when the date and time
+   * @return the date and time with 0 seconds and minutes including rounded seconds
+   */
+  static LocalDateTime roundedMinute(LocalDateTime when) {
+    final double minute = when.getMinute();
+    final double second = when.getSecond();
+    return when.withMinute((int) (minute + Math.round(second / 60))).withSecond(0);
   }
 }
