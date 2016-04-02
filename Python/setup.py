55f59d9360d87d9a5c7d5eb503fe348412bc425d
Matthew Crenshaw <sgt@squig.gs>
2016-04-02 18:09:35 -0400

    prepping for package-worthiness

--- Python/setup.py
+++ Python/setup.py
@@ -2,24 +2,27 @@ from setuptools import setup
 
 
 setup(
-    version='1.0.0',
-    name='adhan',
+    version='0.1.0',
+    name='adhan-batoulapps',
     description='Islamic Prayer Times Calculations',
-    long_description='Some longer description',
+    long_description='Port of Batoul Apps\' Swift Adhan library',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.5',
         'Topic :: Religion',
         'Topic :: Scientific/Engineering :: Astronomy'
     ],
-    keywords='adhan prayer time fajr sunrise dhuhr asr maghrib sunset isha muslim islam',
+    keywords='adhan prayer time fajr sunrise dhuhr asr maghrib sunset isha muslim islam salah',
     url='http://github.com/batoulapps/adhan',
     author='Matthew Crenshaw',
     author_email='matthew.crenshaw@batoulapps.com',
     license='MIT',
-    packages=['adhan'],
-    include_package_data=True,
+    packages=['adhan-batoulapps'],
+    include_package_data=False,
     zip_safe=False,
     test_suite='nose.collector',
     tests_require=['nose']
